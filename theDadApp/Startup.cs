ea2065f12c9c205e9bea6ebc71ce704207b0560d
Ima Dev <imadev@benko.com>
2021-08-11 11:58:07 -0500

    Solution: App Insights

--- theDadApp/Startup.cs
+++ theDadApp/Startup.cs
@@ -24,6 +24,7 @@ namespace theDadApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
